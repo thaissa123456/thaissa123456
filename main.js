88704c8b4c620f873b576ca2be8904b3b15d7648
thaissa123456 <148073084+thaissa123456@users.noreply.github.com>
2023-11-06 08:20:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+Function tocaSom (idElementoAudio){ 
+   document.querySelector(idElementoAudio).play() 
+}
 
+const listadeTeclas = document. querySelectorA11 ('. tecla'); 
+
+let contador = 0; 
+
+for (let contador = O; contador < listaDeTeclas.length; contador++) {
+
+const tecla = listaDeTeclas [contador];
+  const instrumento = tecla.classList[1]; 
+  const idAudio = '#som ${instrumento]* • //template string 
+    
+    tecla.onclick = function () {
+      tocaSom (idAudio);
+    }
+
+}
